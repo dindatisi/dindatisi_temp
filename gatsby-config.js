b44380e737cd7bce50c599477ddf828da47bd4f1
Dinda Tisi Calista <dindatcalista@gmail.com>
2020-06-06 19:33:36 +0200

    add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-108869482-2",
       },
     },
     `gatsby-plugin-sitemap`,
